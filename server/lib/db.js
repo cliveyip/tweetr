acd703537cc4a970263ae1d8ad9c64542b4dda72
Clive Yip <yipsifu@gmail.com>
2016-10-21 18:15:14 +0000

    Heroku deployment: fix mongodb issue

--- server/lib/db.js
+++ server/lib/db.js
@@ -5,7 +5,8 @@
 
 // Mongo DB code
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+//const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const dbMethods = {
 
